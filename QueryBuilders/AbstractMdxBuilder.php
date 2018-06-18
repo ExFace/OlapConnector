2464f092375692deff880789d33c821c2bbc9f01
Andrej Kabachnik <andrej_ka@gmx.de>
2018-06-18 14:19:42 +0200

    NEW data address property MDX_MEMBER_PROPERTY + docs

--- QueryBuilders/AbstractMdxBuilder.php
+++ QueryBuilders/AbstractMdxBuilder.php
@@ -12,6 +12,39 @@ use exface\Core\DataTypes\StringDataType;
 use exface\Core\CommonLogic\QueryBuilder\QueryPartFilter;
 use exface\Core\DataTypes\SortingDirectionsDataType;
 
+/**
+ * Generic MDX builder.
+ * 
+ * ## Data addresses of objects
+ * 
+ * Each cube should be represented by a single meta object. The data address is the cube name: e.g. `[My Cube]`.
+ * 
+ * Technically, it is possible to have multiple meta objects based on the same cube, but putting
+ * attributes of these objects together in a single data widget will result in multiple MDX queries.
+ * 
+ * ## Data addresses of attributes
+ * 
+ * Data addresses for attributes are member expressions: e.g. `[Measures].[My Measure]`.
+ * 
+ * The query builder places attributes automatically on the respective axes, so there is no need to
+ * specify, whether an attribute is a measure or a dimension.
+ * 
+ * ### Data address properties
+ * 
+ * - `MDX_MEMBER_PROPERTY` - allows to address a specific property of a member instead of the member 
+ * name, which is used by default: e.g. [Dimension].[Dimension].CurrentMember.PROPERTIES["..."]. 
+ * Possible values depend on the OLAP engine used. However, most will support `KEY`, so setting this 
+ * option to `KEY` will select/filter member keys.
+ * 
+ * ## Known limitations
+ * 
+ * - No total count of rows available when using pagination. The problem is, that there is no efficient
+ * way to calculate the number of non-empty values in an MDX query. Pagination can be easily done via
+ * SUBSET(), but there is no way to find out, how many pages there are. 
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 abstract class AbstractMdxBuilder extends AbstractQueryBuilder
 {
     const INDENT = '    ';
@@ -25,6 +58,8 @@ abstract class AbstractMdxBuilder extends AbstractQueryBuilder
     private $filtersOnAxes = [];
     
     private $with = [];
+    
+    private $resultColumnAliases = [];
     
     /**
      * 
@@ -42,24 +77,20 @@ abstract class AbstractMdxBuilder extends AbstractQueryBuilder
         // there is another page.
         
         $originalLimit = $this->getLimit();
-        $this->setLimit($originalLimit+1, $this->getOffset());
-        
-        $addressMapping = [];
-        foreach ($this->getAttributes() as $qpart) {
-            if ($qpart->getDataAddress() !== $qpart->getAlias()) {
-                $addressMapping[$qpart->getDataAddress()][] = $qpart->getAlias();
-            }
+        if ($originalLimit > 0) {
+            $this->setLimit($originalLimit+1, $this->getOffset());
         }
         
         $query = new MdxDataQuery($this->buildMdxSelect());
         $query = $data_connection->query($query);
         
         $this->resultRows = $query->getResultArray();
-        if (! empty($addressMapping)) {
+        $aliasMappings = $this->getResultColumnAliases();
+        if (! empty($aliasMappings)) {
             foreach ($this->resultRows as $nr => $row) {
-                foreach ($addressMapping as $dataAddress => $aliases) {
+                foreach ($aliasMappings as $columnName => $aliases) {
                     foreach ($aliases as $alias) {
-                        $row[$alias] = $row[$dataAddress];
+                        $row[$alias] = $row[$columnName];
                     }
                 }
                 $this->resultRows[$nr] = $row;
@@ -124,6 +155,18 @@ MDX;
                 continue;
             }
             $selects = array_merge($selects, $this->buildMdxSelectClauses($qpart));  
+            
+            if ($qpart->getDataAddress() !== $qpart->getAlias()) {
+                $this->addResultColumnAlias($qpart->getAlias(), $qpart->getDataAddress());
+            }
+        }
+        
+        foreach (array_keys($this->with) as $member) {
+            $selects[] = $member;
+            $measure = StringDataType::startsWith($member, '[Measures]') ? $member : "[Measures].[$member]";
+            if ($measure !== $member) {
+                $this->addResultColumnAlias($member, $measure);
+            }
         }
         
         if (! empty($selects)) {
@@ -171,6 +214,18 @@ MDX;
             }
             
             $selects[] = implode(",\n".$indent, $qpartSelects);
+            
+            // Check, if we are actually interested in the member name or another property
+            if ($prop = $qpart->getDataAddressProperty('MDX_MEMBER_PROPERTY')) {
+                // If a property must be selected, we need a calculated member.
+                $member = $this->sanitizeMemberName($qpart->getAlias());
+                $this->addWith($member, "MEMBER {$member} AS {$this->getDimensionOfMember($qpart->getDataAddress())}.CurrentMember.PROPERTIES(\"{$prop}\")");
+            } else {
+                // If the value itself is of interest, use the data address
+                if ($qpart->getDataAddress() !== $qpart->getAlias()) {
+                    $this->addResultColumnAlias($qpart->getAlias(), $qpart->getDataAddress());
+                }
+            }
         }
         
         if (! empty($selects)) {
@@ -231,12 +286,13 @@ MDX;
     {
         $selects = [];
         foreach ($filters as $filter) {
+            $property = $filter->getDataAddressProperty('MDX_MEMBER_PROPERTY') ? strtoupper($filter->getDataAddressProperty('MDX_MEMBER_PROPERTY')) : 'Name';
             switch ($filter->getComparator()) {
                 case EXF_COMPARATOR_IS:
-                    $selects[] = "FILTER({$member}, Instr({$this->getDimensionOfMember($member)}.CurrentMember.Name, '{$filter->getCompareValue()}') > 0)";
+                    $selects[] = "FILTER({$member}, Instr({$this->getDimensionOfMember($member)}.CurrentMember.{$property}, '{$filter->getCompareValue()}') > 0)";
                     break;
                 case EXF_COMPARATOR_IS_NOT:
-                    $selects[] = "FILTER({$member}, Instr({$this->getDimensionOfMember($member)}.CurrentMember.Name, '{$filter->getCompareValue()}') = 0)";
+                    $selects[] = "FILTER({$member}, Instr({$this->getDimensionOfMember($member)}.CurrentMember.{$property}, '{$filter->getCompareValue()}') = 0)";
                     break;
                 case EXF_COMPARATOR_GREATER_THAN:
                 case EXF_COMPARATOR_GREATER_THAN_OR_EQUALS:
@@ -245,12 +301,20 @@ MDX;
                     $selects[] = "FILTER({$member}, {$member} {$filter->getComparator()} {$filter->getCompareValue()}))";
                     break;
                 case EXF_COMPARATOR_EQUALS:
-                    $selects[] = $member . '.[' . $filter->getCompareValue() . ']';
+                    switch ($property) {
+                        case 'KEY' : $selects[] = $member . '.&[' . $filter->getCompareValue() . ']'; break;
+                        case 'Name' : $selects[] = $member . '.[' . $filter->getCompareValue() . ']'; break;
+                        default: throw new QueryBuilderException('Cannot build MDX equals-filter for "' . $filter->getAlias() . '": filtering over member property "' . $property . '" not supported!');
+                    }
                     break;
                 case EXF_COMPARATOR_IN:
                     $values = explode($filter->getAttribute()->getValueListDelimiter(), $filter->getCompareValue());
                     foreach ($values as $val) {
-                        $selects[] = $member . '.[' . $val . ']';
+                        switch ($property) {
+                            case 'KEY' : $selects[] = $member . '.&[' . $val . ']'; break;
+                            case 'Name' : $selects[] = $member . '.[' . $val . ']'; break;
+                            default: throw new QueryBuilderException('Cannot build MDX in-filter for "' . $filter->getAlias() . '": filtering over member property "' . $property . '" not supported!');
+                        }
                     }
                     break;
                 default:
@@ -421,9 +485,9 @@ MDX;
      * @param string $statement
      * @return AbstractMdxBuilder
      */
-    protected function addWith(string $statement) : AbstractMdxBuilder
+    protected function addWith(string $memberName, string $statement) : AbstractMdxBuilder
     {
-        $this->with[] = $statement;
+        $this->with[$memberName] = $statement;
         return $this;
     }
     
@@ -445,5 +509,21 @@ MDX;
             $rowSet = "ORDER({$rowSet}, {$sortBy}, {$dir})";
         }
         return $rowSet;
+    }
+    
+    protected function sanitizeMemberName(string $name) : string
+    {
+        return str_replace(['[', ']', ' '], '_', $name);
+    }
+    
+    protected function getResultColumnAliases() : array
+    {
+        return $this->resultColumnAliases;
+    }
+    
+    protected function addResultColumnAlias(string $alias, string $resultColumnName) : AbstractMdxBuilder
+    {
+        $this->resultColumnAliases[$resultColumnName][] = $alias;
+        return $this;
     }
 }
